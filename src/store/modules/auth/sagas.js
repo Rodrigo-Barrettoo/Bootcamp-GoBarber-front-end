5f13a17e4f7ca96e7c777a44d646e6b7a1fcf6a8
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2020-03-10 14:19:17 -0300

    Requisições autenticadas

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -19,8 +19,11 @@ export function* signIn({ payload }) {
 
     if (!user.provider) {
       toast.error('Usuario não é prestado!');
+      yield put(signFailure());
       return;
     }
+
+    api.defaults.headers.Authorization = `Bearer ${token}`;
 
     yield put(signInSuccess(token, user));
 
@@ -31,4 +34,40 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    toast.success('Cadastro realizado com sucesso, Faça login!');
+
+    history.push('/');
+  } catch (error) {
+    toast.error('Falha no Cadastro, verifique seus dados');
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) {
+    return;
+  }
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
