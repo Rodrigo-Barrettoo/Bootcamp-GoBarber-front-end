c2fc72175fb3d466f4eb365cfeb6e4e4c76d3311
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2020-03-12 09:59:04 -0300

    Estilização do Dashboard

--- src/pages/Dashbord/index.js
+++ src/pages/Dashbord/index.js
@@ -1,9 +1,40 @@
 import React from 'react';
-import api from '~/services/api';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+// import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashbord() {
-  api.get('appointments');
-  return <h1>Dashbord</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="submit">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31 de maio</strong>
+        <button type="submit">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Rodrigo</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Rodrigo</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Rodrigo</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
